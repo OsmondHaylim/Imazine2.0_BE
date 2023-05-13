a1538695e35cbeb8fc3770533a3b919d58d50b90
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-13 23:27:34 +0700

    add structs and methods for conversion

--- models/article_category.go
+++ models/article_category.go
@@ -5,3 +5,16 @@ type ArticleCategory struct {
 	Name  string `gorm:"notNull" json:"name"`
 	Users []User `gorm:"many2many:has_article_edit_access;" json:"users"`
 }
+
+// not for database table, but for response conversion to match API spec
+type ArticleCategorySmall struct {
+	ID   int    `gorm:"primaryKey;autoIncrement;notNull" json:"id"`
+	Name string `gorm:"notNull" json:"name"`
+}
+
+func ToArticleCategorySmall(articleCategory ArticleCategory) ArticleCategorySmall {
+	return ArticleCategorySmall{
+		ID:   articleCategory.ID,
+		Name: articleCategory.Name,
+	}
+}
