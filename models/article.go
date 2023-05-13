4a7961efb0619044f64aef852ce34318f30203f5
AncuL001 <ancul001@gmail.com>
2023-05-13 23:27:34 +0700

    add structs and methods for conversion

--- models/article.go
+++ models/article.go
@@ -16,3 +16,28 @@ type Article struct {
 	CreatedAt       time.Time       `gorm:"notNull" json:"created_at"`
 	ViewCount       int             `gorm:"notNull;default:0" json:"view_count"`
 }
+
+// not for database table, but for response conversion to match API spec
+type ArticleSmall struct {
+	ID              int             `gorm:"primaryKey;autoIncrement" json:"id"`
+	Creator         UserSmall       `gorm:"notNull;foreignKey:CreatorID" json:"creator"`
+	Category        ArticleCategorySmall `gorm:"notNull;foreignKey:CategoryID" json:"category"`
+	Title           string          `gorm:"notNull" json:"title" form:"title"`
+	CoverImageLink  string          `gorm:"notNull" json:"cover_image_link"`
+	MarkdownContent string          `gorm:"notNull" json:"markdown_content" form:"markdown_content"`
+	CreatedAt       time.Time       `gorm:"notNull" json:"created_at"`
+	ViewCount       int             `gorm:"notNull;default:0" json:"view_count"`
+}
+
+func ToArticleSmall(article Article) ArticleSmall {
+	return ArticleSmall{
+		ID:              article.ID,
+		Creator:         ToUserSmall(article.Creator),
+		Category:        ToArticleCategorySmall(article.Category),
+		Title:           article.Title,
+		CoverImageLink:  article.CoverImageLink,
+		MarkdownContent: article.MarkdownContent,
+		CreatedAt:       article.CreatedAt,
+		ViewCount:       article.ViewCount,
+	}
+}
--- models/user.go
+++ models/user.go
@@ -11,3 +11,20 @@ type User struct {
 	Token                string            `json:"token"`
 	HasArticleEditAccess []ArticleCategory `gorm:"many2many:has_article_edit_access;" json:"has_article_edit_access"`
 }
+
+// not for database table, but for response conversion to match API spec
+type UserSmall struct {
+	ID                 int    `json:"id"`
+	Name               string `json:"name"`
+	NPM                string `json:"npm"`
+	ProfilePictureLink string `json:"profile_picture_link"`
+}
+
+func ToUserSmall(user User) UserSmall {
+	return UserSmall{
+		ID:                 user.ID,
+		Name:               user.Name,
+		NPM:                user.NPM,
+		ProfilePictureLink: user.ProfilePictureLink,
+	}
+}
