6fb8930f52677a6281adc026c2180e7c4aacee60
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-13 19:20:48 +0700

    create models

---
+++ models/user.go
@@ -0,0 +1,12 @@
+package models
+
+type User struct {
+	ID                   int               `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name                 string            `gorm:"notNull" json:"name"`
+	NPM                  string            `gorm:"notNull" json:"npm"`
+	ProfilePictureLink   string            `json:"profile_picture_link"`
+	Email                string            `gorm:"notNull" json:"email"`
+	Password             string            `gorm:"notNull" json:"password"`
+	IsAdmin              bool              `gorm:"notNull;default:false" json:"is_admin"`
+	HasArticleEditAccess []ArticleCategory `gorm:"many2many:has_article_edit_access;" json:"has_article_edit_access"`
+}
