6859c05b21d010c1ad0044364e62c2a7efff1625
AncuL001 <ancul001@gmail.com>
2023-05-13 21:27:58 +0700

    fix errors

--- models/article.go
+++ models/article.go
@@ -10,7 +10,8 @@ type Article struct {
 	CategoryID      int             `gorm:"notNull" json:"category_id"`
 	Category        ArticleCategory `json:"category"`
 	Title           string          `gorm:"notNull" json:"title"`
-	MarkdownContent string          `gorm:"notNull" json:"markdown_content "`
+	CoverImageLink  string          `gorm:"notNull" json:"cover_image_link"`
+	MarkdownContent string          `gorm:"notNull" json:"markdown_content"`
 	CreatedAt       time.Time       `gorm:"notNull" json:"created_at"`
 	ViewCount       int             `gorm:"notNull;default:0" json:"view_count"`
 }
