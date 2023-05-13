3ccb3db0725d1ec929aa05108772a57c560ab85a
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-13 22:52:06 +0700

    move code across files

---
+++ routes/articles.go
@@ -0,0 +1,97 @@
+package routes
+
+import (
+	"fmt"
+	"imazine/models"
+	"imazine/storage"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm/clause"
+)
+
+
+func CreateArticle(context *fiber.Ctx) error{
+	article := new(models.Article)
+
+	if err := context.BodyParser(article); err != nil {
+		return context.Status(400).JSON(err.Error())
+	}
+
+	storage.DB.Db.Create(&article)
+
+	var result models.Article
+
+	storage.DB.Db.Preload(clause.Associations).First(&result, article.ID)
+
+	return context.Status(200).JSON(&fiber.Map{
+		"message": "Article created",
+		"article": result,
+	})
+}
+
+func GetArticle(context *fiber.Ctx) error{
+	artM := &[]models.Article{}
+
+	err := storage.DB.Db.Find(artM).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message":"Could not get articles"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message":	"Articles fetched successfully", 
+		"data":		artM,
+	})
+	return err
+}
+
+func DeleteArticle(context *fiber.Ctx) error{
+	artM := models.Article{}
+	id := context.Params("id")
+	if id == ""{
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message":"ID cannot be empty",
+		})
+		return nil
+	}
+
+	err := storage.DB.Db.Delete(artM, id)
+
+	if err.Error != nil{
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"message":"Could not delete article",
+		})
+		return err.Error
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message":"Article deleted",
+	})
+	return nil
+}
+
+func GetArticleByID(context *fiber.Ctx) error{
+	id := context.Params("id")
+	artM := &models.Article{}
+	if id == ""{
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message":"ID cannot be empty",
+		})
+		return nil
+	}
+
+	fmt.Println("ID = ", id)
+	err := storage.DB.Db.Preload(clause.Associations).First(artM, id).Error
+	if err != nil{
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"message":"Couldn't get article",
+		})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message":"Article ID fetched",
+		"data":artM,
+	})
+	return nil
+}
--- storage/postgres.go
+++ storage/postgres.go
@@ -1,7 +1,9 @@
 package storage
 
-import(
+import (
 	"fmt"
+	"imazine/models"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,11 +17,32 @@ type Config struct{
 	SSLMode		string
 }
 
-func NewConnection(config *Config)(*gorm.DB, error){
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func MigrateModels(db *gorm.DB) {
+	db.AutoMigrate(models.ArticleCategory{})
+	db.AutoMigrate(models.User{})
+	db.AutoMigrate(models.Article{})
+}
+
+func ConnectDB(config *Config) {
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode,
 	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	return db, err
+
+	if err != nil {
+		panic("Failed to connect to database. \n" + err.Error())
+	}
+
+	MigrateModels(db)
+
+	DB = Dbinstance{
+		Db: db,
+	}
 }
