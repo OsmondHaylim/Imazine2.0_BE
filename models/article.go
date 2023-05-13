e5a76a84dd20280aaaf57c5150435213731d2887
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-13 22:48:35 +0700

    fix missing and different items

--- models/article.go
+++ models/article.go
@@ -6,12 +6,13 @@ import (
 
 type Article struct {
 	ID              int             `gorm:"primaryKey;autoIncrement" json:"id"`
-	Author          string          `gorm:"notNull" json:"author"`
-	CategoryID      int             `gorm:"notNull" json:"category_id"`
-	Category        ArticleCategory `json:"category"`
-	Title           string          `gorm:"notNull" json:"title"`
+	CreatorID       int             `gorm:"notNull" json:"creator_id" form:"creator_id"`
+	Creator         User            `gorm:"notNull;foreignKey:CreatorID" json:"creator"`
+	CategoryID      int             `gorm:"notNull" json:"category_id" form:"category_id"`
+	Category        ArticleCategory `gorm:"notNull;foreignKey:CategoryID" json:"category"`
+	Title           string          `gorm:"notNull" json:"title" form:"title"`
 	CoverImageLink  string          `gorm:"notNull" json:"cover_image_link"`
-	MarkdownContent string          `gorm:"notNull" json:"markdown_content"`
+	MarkdownContent string          `gorm:"notNull" json:"markdown_content" form:"markdown_content"`
 	CreatedAt       time.Time       `gorm:"notNull" json:"created_at"`
 	ViewCount       int             `gorm:"notNull;default:0" json:"view_count"`
 }
