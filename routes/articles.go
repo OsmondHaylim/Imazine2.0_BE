026665db773482dde3cd36471992612ed0d676e4
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-18 15:22:42 +0700

    update article functionality

--- routes/articles.go
+++ routes/articles.go
@@ -73,6 +73,62 @@ func CreateArticle(context *fiber.Ctx) error{
 	return context.Status(200).JSON(models.ToArticleSmall(result))
 }
 
+func UpdateArticle(context *fiber.Ctx) error {
+	newArticleData := new(models.Article)
+
+	if err := context.BodyParser(newArticleData); err != nil {
+		return context.Status(400).JSON(err.Error())
+	}
+
+	id := context.Params("id")
+
+	currArticle := new(models.Article)
+	if err := storage.DB.Db.First(&currArticle, id).Error; err != nil {
+		return context.Status(400).JSON(err.Error)
+	}
+
+	if file, err := context.FormFile("cover_image"); err == nil {
+		context.SaveFile(file, fmt.Sprintf("./download_cache/%s", file.Filename))
+
+		values := map[string]io.Reader{
+			"image": utils.MustOpen(fmt.Sprintf("download_cache/%s", file.Filename)), // lets assume its this file
+		}
+		
+		res, err := utils.Upload("https://api.imgbb.com/1/upload?key=7b39ff8818a667ee516b470fd8bcbd09", values)
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return context.Status(400).JSON(err.Error())
+		}
+		bodyString := string(bodyBytes)
+
+		type ExtractImgUrl struct {
+			Data struct {
+				Url string `json:"url"`
+			}
+		}
+
+		var imgUrl ExtractImgUrl
+		err = json.Unmarshal([]byte(bodyString), &imgUrl)
+
+		err = os.Remove(fmt.Sprintf("download_cache/%s", file.Filename))  // remove a single file
+		if err != nil {
+			return context.Status(400).JSON(err.Error())
+		}
+
+		newArticleData.CoverImageLink = imgUrl.Data.Url
+	}
+
+	err := storage.DB.Db.Model(&currArticle).Updates(newArticleData).Error
+	if err != nil {
+		return context.Status(400).JSON(err.Error())
+	}
+
+	var result models.Article
+	storage.DB.Db.Preload(clause.Associations).First(&result, currArticle.ID)
+
+	return context.Status(http.StatusOK).JSON(models.ToArticleSmall(result))
+}
+
 func GetArticle(context *fiber.Ctx) error{
 	articles := &[]models.Article{}
 	query := storage.DB.Db.Preload(clause.Associations).Order("created_at desc")
