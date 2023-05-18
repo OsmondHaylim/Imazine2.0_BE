aeb0045a69b6d666746e0e9af865870ae00cc8a2
AncuL001 <ancul001@gmail.com>
2023-05-18 15:22:15 +0700

    add id params to delete article

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func setUpRoutes(app *fiber.App) {
 	app.Get("/articles", routes.GetArticle)
 	app.Get("/articles/:id", routes.GetArticleByID)
 	app.Post("/articles", routes.CreateArticle)
-	app.Delete("/articles", routes.DeleteArticle)
+	app.Delete("/articles/:id", routes.DeleteArticle)
 
 	app.Get("/creators/articles/:id", routes.GetArticleByCategory)
 }
