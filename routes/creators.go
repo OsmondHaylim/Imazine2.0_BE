575b05430b4e98c89e6cfbba57539ca97c1f08a7
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-18 08:11:48 +0700

    implement searching articles

--- routes/creators.go
+++ routes/creators.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"imazine/models"
 	"imazine/storage"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm/clause"
@@ -13,7 +14,13 @@ func GetArticleByCategory(context *fiber.Ctx) error{
 
 	category := context.Params("id")
 
-	err := storage.DB.Db.Preload(clause.Associations).Order("created_at desc").Where("category_id = ?", category).Find(&articles).Error
+	query := storage.DB.Db.Preload(clause.Associations).Order("created_at desc").Where("category_id = ?", category)
+
+	if search := context.Query("search"); len(search) >= 3 {
+		query = query.Where("lower(title) LIKE ?", "%" + strings.ToLower(search) + "%")
+	}
+
+	err := query.Find(&articles).Error
 	if err != nil {
 		return context.Status(400).JSON(err)
 	}
