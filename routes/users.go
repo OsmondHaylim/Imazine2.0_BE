4324ce86ebd786661d40c2c88caecee874d0f8f5
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-20 15:08:37 +0700

    make search query optional + order by npm

--- routes/users.go
+++ routes/users.go
@@ -9,19 +9,16 @@ import (
 )
 
 func SearchUser(context *fiber.Ctx) error {
-	search := context.Query("search")
-	if len(search) < 3 {
-		return context.Status(200).JSON([]string{})
-	}
-	
-	users := &[]models.UserSmall{}
-
 	query := storage.DB.Db.Model(&models.User{})
 
-	query = query.Where("lower(name) LIKE ?", "%" + strings.ToLower(search) + "%").
-				  Or("npm LIKE ?", "%" + search + "%")
+	if search := context.Query("search"); len(search) >= 3 {
+		query = query.Where("lower(name) LIKE ?", "%" + strings.ToLower(search) + "%").
+					  Or("npm LIKE ?", "%" + search + "%")
+	}
 
-	if err := query.Find(users).Error; err != nil {
+	users := &[]models.UserSmall{}
+
+	if err := query.Order("npm asc").Find(users).Error; err != nil {
 		return context.Status(400).JSON(err)
 	}
 
