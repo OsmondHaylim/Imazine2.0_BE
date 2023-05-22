b4f6c12e6ef43d9cd1adf65c44a27b7870be9d4d
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-05-22 23:12:16 +0700

    add, update, get request

--- models/portal.go
+++ models/portal.go
@@ -6,8 +6,24 @@ type Request struct{
 	ID 				int 			`gorm:"primaryKey;autoIncrement" json:"id"`
 	CategoryID      int             `gorm:"notNull" json:"category_id" form:"category_id"`
 	Category        ArticleCategory `gorm:"notNull;foreignKey:CategoryID" json:"category"`
+	ContentHeader 	string          `gorm:"notNull" json:"content_header" form:"content_header"`
 	Content 		string          `gorm:"notNull" json:"content" form:"content"`
 	Status 			string			`gorm:"notNull" json:"status"`
 	CreatedAt   	time.Time       `gorm:"notNull" json:"created_at"`
 	Deadline		time.Time		`gorm:"notNull" json:"deadline"`
+}
+type RequestSmall struct{
+	ID 				int 			`gorm:"primaryKey;autoIncrement" json:"id"`
+	ContentHeader 	string          `gorm:"notNull" json:"content_header" form:"content_header"`
+	Status 			string			`gorm:"notNull" json:"status"`
+	Deadline		time.Time		`gorm:"notNull" json:"deadline"`
+}
+
+func ToRequestSmall(request Request) RequestSmall {
+	return RequestSmall{
+		ID:              	request.ID,
+		ContentHeader:   	request.ContentHeader,
+		Status:        		request.Status,
+		Deadline:           request.Deadline,
+	}
 }
--- routes/portal.go
+++ routes/portal.go
@@ -22,15 +22,33 @@ func CreateRequest(context *fiber.Ctx) error{
 		return context.Status(400).JSON(err.Error())
 	}
 
-	
+	return context.Status(200).JSON(models.ToRequestSmall(request))
 }
 
 func UpdateRequest(context *fiber.Ctx) error {
-	return nil
+	request := new(models.Request)
+
+	storage.DB.Db.First(request, context.Params("id"))
+
+	if err := context.BodyParser(request); err != nil {
+		return context.Status(400).JSON(err.Error())
+	}
+
+	storage.DB.Db.Save(&request)
+
+	return context.Status(200).JSON(models.ToRequestSmall(request))
 }
 
 func GetRequest(context *fiber.Ctx) error{
-	return nil
+	requests := &[]models.Request{}
+	query := storage.DB.Db.Preload(clause.Associations).Order("deadline asc")
+
+	err := query.Find(requests).Error
+	if err != nil {
+		return context.Status(400).JSON(err)
+	}
+
+	return context.Status(200).JSON(requests)
 }
 
 func DeleteRequest(context *fiber.Ctx) error{
